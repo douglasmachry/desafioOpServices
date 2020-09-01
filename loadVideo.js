cc93903c841093870512b78f6fb40afe6d432270
Douglas Machry <machry3@gmail.com>
2020-09-01 14:00:44 -0300

    correção no carregamento da página

--- loadVideo.js
+++ loadVideo.js
@@ -1,24 +1,13 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "https://eitri.opservices.com/video.html",
-        type: 'GET',
-        success: function(res) {
-            var html = res;
-            $("body").html(html);
-            $("#oi a").click(function(e) {
-                e.preventDefault();
-                var video = $(this).attr("href");
-                $.ajax({
-                    url: video,
-                    type: "GET",
-                    success: function(urlVideo) {
-                        $("#videoaqui").html(urlVideo);
-                    }
-                })
-
-
-            })
-        }
-    })
-    $("body").html()
+    $("#oi a").click(function(e) {
+        e.preventDefault();
+        var video = $(this).attr("href");
+        $.ajax({
+            url: video,
+            type: "GET",
+            success: function(urlVideo) {
+                $("#videoaqui").html(urlVideo);
+            }
+        })
+    });
 });
